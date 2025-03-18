ecb89099b18fd6d6cf81db8e69e8ccbd81a2d4e8
FyN8 <fyn8gmr@gmail.com>
2025-03-19 00:52:26 +0530

    Data Ingestion Added

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Log test")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f"Starting {STAGE_NAME}")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"{STAGE_NAME} completed successfully")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
